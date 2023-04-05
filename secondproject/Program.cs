765c574aa4494c882553e718bd932baea53cb1b8
indhu <indhu@LAPTOP-761FVFV7>
2023-04-05 13:50:35 +0530

    adding the files

---
+++ secondproject/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace secondproject
+{
+    public static class secondprojectClass
+    {
+
+        public static void SayHello()
+        {
+
+            Console.WriteLine("Hello From Project 2");
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+    }
+}
