29a7c6a0f8d859a7edcb9a20c08e92e13626f10f
indhu <indhu@LAPTOP-761FVFV7>
2023-04-20 12:03:46 +0530

    adding function2

---
+++ function2/secondproject/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace secondproject
+{
+    public static class secondprojectClass
+    {
+
+        public static void SayHello()
+        {
+
+             Trace.Listeners.Add(new TextWriterTraceListener(Console.Out));
+       Trace.AutoFlush = true;
+       Trace.Indent();
+       Trace.WriteLine("Entering hello function");
+       Console.WriteLine("Hello World.");
+       Trace.WriteLine("Exiting hello function");
+       Trace.Unindent();
+
+        }
+        static void Main(string[] args)
+        {
+            Trace.Listeners.Add(new TextWriterTraceListener(Console.Out));
+       Trace.AutoFlush = true;
+       Trace.Indent();
+       Trace.WriteLine("Entering Main");
+       Console.WriteLine("Hello World.");
+       Trace.WriteLine("Exiting Main");
+       Trace.Unindent();
+        }
+
+
+    }
+}
